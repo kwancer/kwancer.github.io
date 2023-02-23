c9fbbadc04c846e69096c989072f80804f01493d
Krzysztof Wancerski <79153882+kwancer@users.noreply.github.com>
2023-02-23 20:15:33 +0000

    added box

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 console.log("hello")
+var popup = document.getElementsByClassName('popup')[0];
+var closeBtn = document.getElementById('close-btn');
+var content = document.getElementsByClassName("content")[0];
 
 
 function updateTime(){
@@ -8,8 +11,22 @@ function updateTime(){
       });
     timeElement = document.getElementById("time")
     timeElement.innerHTML = n;
-    
 }
 
-setInterval(updateTime, 50);
 
+console.log(popup)
+
+closeBtn.addEventListener('click', () => {
+  popup.style.display = 'none';
+  content.style.display = 'block';
+  console.log("wowowowowow")
+});
+
+window.addEventListener('load', () => {
+  popup.style.display = 'flex';
+  content.style.display = 'none';
+  console.log("wowowowowow")
+});
+
+
+setInterval(updateTime, 50);
