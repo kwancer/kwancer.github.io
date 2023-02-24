8ef15f54ed2e2437aad45a21794421fed74b7fe2
Krzysztof Wancerski <79153882+kwancer@users.noreply.github.com>
2023-02-24 13:07:24 +0000

    improved

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-console.log("hello")
-var popup = document.getElementsByClassName('popup')[0];
-var closeBtn = document.getElementById('close-btn');
-var content = document.getElementsByClassName("content")[0];
-
-
-
 function updateTime(){
     var d = new Date();
     var n = d.toLocaleTimeString([], {
@@ -15,28 +8,6 @@ function updateTime(){
 }
 
 
-console.log(popup)
-
-closeBtn.addEventListener('click', () => {
-  popup.style.display = 'none';
-  content.style.display = 'block';
-  console.log("wowowowowow")
-});
-
-window.addEventListener('load', () => {
-    popup.style.display = 'flex';
-    content.style.display = 'none';
-    console.log("wowowowowow")
-});
-
-
 setInterval(updateTime, 50);
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  
-});
 
-var marker = new mapboxgl.Marker()
-marker.setLngLat([-0.118092, 51.509865])
-marker.addTo(map);
